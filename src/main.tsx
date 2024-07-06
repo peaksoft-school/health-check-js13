4700ed3aec243c9ea059d89c37b2e04cbf119df6
AmanturDev11 <ismailovamantur22@gmail.com>
2024-07-06 22:24:55 +0600

    create toastify message components

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import Themes from "./theme/ThemeProvider.tsx";
+import Notification from "./components/UI/Notification.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<Themes>
+			<Notification />
 			<App />
 		</Themes>
 	</React.StrictMode>
