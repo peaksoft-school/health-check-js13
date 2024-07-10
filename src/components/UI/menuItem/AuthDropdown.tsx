230909761bc2f59fa2efcaf0da3d807724a7686e
AmanturDev11 <ismailovamantur22@gmail.com>
2024-07-10 16:34:06 +0600

    ..

--- src/components/UI/menuItem/AuthDropdown.tsx
+++ src/components/UI/menuItem/AuthDropdown.tsx
@@ -1,105 +1,105 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import ClickAwayListener from "@mui/material/ClickAwayListener";
-import Grow from "@mui/material/Grow";
-import Paper from "@mui/material/Paper";
-import Popper from "@mui/material/Popper";
-import MenuItem from "@mui/material/MenuItem";
-import MenuList from "@mui/material/MenuList";
-import Stack from "@mui/material/Stack";
-import users from "../../../assets/icons/UserIcon.svg";
-import { styled } from "@mui/material";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+import Stack from '@mui/material/Stack';
+import users from '../../../assets/icons/UserIcon.svg';
+import { styled } from '@mui/material';
 
 export default function AuthDropdown() {
-	const [open, setOpen] = React.useState(false);
-	const anchorRef = React.useRef<HTMLButtonElement>(null);
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef<HTMLButtonElement>(null);
 
-	const handleToggle = () => {
-		setOpen((prevOpen) => !prevOpen);
-	};
+  const handleToggle = () => {
+    setOpen(prevOpen => !prevOpen);
+  };
 
-	const handleClose = (event: Event | React.SyntheticEvent) => {
-		if (
-			anchorRef.current &&
-			anchorRef.current.contains(event.target as HTMLElement)
-		) {
-			return;
-		}
+  const handleClose = (event: Event | React.SyntheticEvent) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
 
-		setOpen(false);
-	};
+    setOpen(false);
+  };
 
-	function handleListKeyDown(event: React.KeyboardEvent) {
-		if (event.key === "Tab") {
-			event.preventDefault();
-			setOpen(false);
-		} else if (event.key === "Escape") {
-			setOpen(false);
-		}
-	}
+  function handleListKeyDown(event: React.KeyboardEvent) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === 'Escape') {
+      setOpen(false);
+    }
+  }
 
-	const prevOpen = React.useRef(open);
-	React.useEffect(() => {
-		if (prevOpen.current === true && open === false) {
-			anchorRef.current!.focus();
-		}
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current!.focus();
+    }
 
-		prevOpen.current = open;
-	}, [open]);
+    prevOpen.current = open;
+  }, [open]);
 
-	return (
-		<Stack direction="row" spacing={2}>
-			<div>
-				<Button
-					ref={anchorRef}
-					id="composition-button"
-					aria-controls={open ? "composition-menu" : undefined}
-					aria-expanded={open ? "true" : undefined}
-					aria-haspopup="true"
-					onClick={handleToggle}>
-					<img
-						style={{ width: "28px", height: "28px" }}
-						src={users}
-						alt="User"
-					/>
-				</Button>
-				<Popper
-					open={open}
-					anchorEl={anchorRef.current}
-					role={undefined}
-					placement="bottom-start"
-					transition
-					style={{ zIndex: 2500 }}
-					disablePortal>
-					{({ TransitionProps, placement }) => (
-						<Grow
-							{...TransitionProps}
-							style={{
-								transformOrigin:
-									placement === "bottom-start" ? "left top" : "left bottom",
-							}}>
-							<Paper>
-								<ClickAwayListener onClickAway={handleClose}>
-									<MenuList
-										autoFocusItem={open}
-										id="composition-menu"
-										aria-labelledby="composition-button"
-										onKeyDown={handleListKeyDown}>
-										<ButtonMui onClick={handleClose}>Войти</ButtonMui>
-										<ButtonMui onClick={handleClose}>Регистрация</ButtonMui>
-									</MenuList>
-								</ClickAwayListener>
-							</Paper>
-						</Grow>
-					)}
-				</Popper>
-			</div>
-		</Stack>
-	);
+  return (
+    <Stack direction="row" spacing={2}>
+      <div>
+        <Button
+          ref={anchorRef}
+          id="composition-button"
+          aria-controls={open ? 'composition-menu' : undefined}
+          aria-expanded={open ? 'true' : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}>
+          <img
+            style={{ width: '28px', height: '28px' }}
+            src={users}
+            alt="User"
+          />
+        </Button>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          placement="bottom-start"
+          transition
+          style={{ zIndex: 2500 }}
+          disablePortal>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === 'bottom-start' ? 'left top' : 'left bottom',
+              }}>
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList
+                    autoFocusItem={open}
+                    id="composition-menu"
+                    aria-labelledby="composition-button"
+                    onKeyDown={handleListKeyDown}>
+                    <ButtonMui onClick={handleClose}>Войти</ButtonMui>
+                    <ButtonMui onClick={handleClose}>Регистрация</ButtonMui>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </div>
+    </Stack>
+  );
 }
 
 const ButtonMui = styled(MenuItem)(() => ({
-	"&:hover": {
-		color: "#048741",
-	},
+  '&:hover': {
+    color: '#048741',
+  },
 }));
