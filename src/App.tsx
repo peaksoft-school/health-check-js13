354ec1dab1e483dfe62bcdd6e079dd6478825a3f
Syimyk <syjmyk907@gmail.com>
2024-07-12 21:08:44 +0600

    create landing service

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react';
+import OurService from './components/landingPage/OurService';
 // import Switcher from "./components/UI/CustomUI/Switcher";
 
 const App: FC = () => {
   return (
-    <div>{/* <Switcher label="Create"  labelPlacement="bottom" /> */}</div>
+    <div>
+      {/* <Switcher label="Create"  labelPlacement="bottom" /> */}
+      <OurService />
+    </div>
   );
 };
 
