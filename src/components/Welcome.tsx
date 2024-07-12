7adf5be6bc06092bf86c2e1e7d89321395e16f08
koichumanovernest <koichumanovernest@gmail.com>
2024-07-12 10:06:43 +0600

    create welcome

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,12 +1,12 @@
 import { styled, Modal as MuiModal, Box } from '@mui/material';
-import image from '../assets/images/Mask group (2).png';
-import Button from './UI/CustomUI/Button';
 import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import Input from './UI/CustomUI/Input';
+import Image from '../assets/images/DoctorMakc.png';
 import MaleFemale from '../assets/icons/MaleFemaleIcon.svg';
 import CallProgres from '../assets/icons/CallProgressIcon.svg';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import Arrow from "../assets/icons/ArrowIcon.svg";
+import Arrow from '../assets/icons/ArrowIcon.svg';
+import Close from '../assets/icons/CloseIcon.svg';
 
 interface IFormTypes {
   name: string;
@@ -55,14 +55,14 @@ const Welcome = () => {
         </div>
 
         <div>
-          <Button onClick={handleOpen} variant="outlined">
+          <StyledButtonMain onClick={handleOpen}>
             Оставьте заявку
-          </Button>
+          </StyledButtonMain>
         </div>
       </StyledContentText>
 
       <div>
-        <img src={image} alt="HealthCheck" />
+        <img src={Image} alt="HealthCheck" />
       </div>
 
       <MuiModal open={isOpen} onClose={handleClose}>
@@ -85,6 +85,7 @@ const Welcome = () => {
                   required: 'Name required',
                 })}
                 icon={MaleFemale}
+                placeholder="Введите имя"
                 label="Как к Вам обратиться?"
               />
 
@@ -94,15 +95,17 @@ const Welcome = () => {
                 })}
                 icon={CallProgres}
                 label="Номер мобильного телефона"
-                placeholder="+(996)"
+                placeholder="+996(___) __-__-__"
                 type="text"
               />
             </StyledInputContent>
 
-            <StyledButton  onClick={handleOpenSecondModal} type="submit">
+            <StyledButton onClick={handleOpenSecondModal} type="submit">
               ОТПРАВИТЬ ЗАЯВКУ
               <StyledImg src={Arrow} alt="" />
             </StyledButton>
+
+            <StyledModalImg onClick={handleClose} src={Close} alt="" />
           </StyledFormContainer>
         </ModalContent>
       </MuiModal>
@@ -110,10 +113,13 @@ const Welcome = () => {
       <MuiModal open={isOpenSecondModal} onClose={handleCloseSecondModal}>
         <StyledSecondModal>
           <h2> Заявка успешно отправлена!</h2>
-          <p>
-            В ближайшее время с вами свяжется администратор для согласования
-            деталей.
-          </p>
+
+          <StyledParagraf>
+            <p>В ближайшее время с вами свяжется администратор</p>
+            <p>для согласования деталей.</p>
+          </StyledParagraf>
+
+          <StyledModalImg onClick={handleCloseSecondModal} src={Close} alt="" />
         </StyledSecondModal>
       </MuiModal>
     </StyledContainer>
@@ -124,57 +130,87 @@ export default Welcome;
 
 const StyledContainer = styled('div')`
   width: 100%;
-  max-width: 1500px;
+  max-width: 1420px;
   min-width: 1200px;
+  margin: 0 auto;
   display: flex;
   justify-content: space-between;
 `;
 
 const StyledContentText = styled('div')`
-  width: 40%;
-  padding: 20px;
+  width: 45%;
   display: flex;
   flex-direction: column;
+  margin-top: 100px;
 `;
 
 const StyledH1 = styled('h1')`
-  font-size: 50px;
+  font-size: 3.700rem;
   line-height: 1.1;
   font-weight: bold;
   margin-bottom: 30px;
   background: linear-gradient(90deg, #30c5cb, #33327e);
-  -webkit-background-clip: text;
+  background-clip: text;
   -webkit-text-fill-color: transparent;
+  font-weight: 900;
+  font-family: 'Manrope', sans-serif;
+`;
+const StyledText = styled('p')`
+  width: 90%;
+  color: #333;
+  margin-bottom: 40px;
+  margin-top: 5px;
+  font-size: 19px;
+  line-height: 1.5;
+  font-family: 'Manrope', sans-serif;
+`;
+
+const StyledModalImg = styled('img')`
+  width: 36px;
+  height: 36px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
 `;
 
 const StyledH2 = styled('h1')`
   margin-top: 40px;
-`;
-const StyledImg = styled("img")`
-margin-left: 30px;
-`
-
-const StyledText = styled('p')`
-  font-size: 18px;
-  line-height: 1.5;
-  color: #333;
-  margin-bottom: 20px;
-  width: 85%;
+  font-family: 'Manrope', sans-serif;
+  font-weight: 500;
 `;
 
-const StyledButton = styled("button")`
+
+const StyledImg = styled('img')`
+  margin-left: 30px;
+`;
+
+
+
+const StyledButton = styled('button')`
   width: 220px;
   height: 44px;
   display: flex;
   justify-content: center;
   align-items: center;
-  border:none;
+  border: none;
   margin-left: 9rem;
   background: #04884c;
   color: white;
-  font-weight: 400;
+  font-weight: 500;
   border-radius: 2rem;
-  padding:8px, 12px, 8px, 24px;
+  padding: 8px, 12px, 8px, 24px;
+  font-family: 'Manrope', sans-serif;
+`;
+
+const StyledButtonMain = styled('button')`
+  border-radius: 30px;
+  padding: 10px 26px;
+  border: 1px solid #048741;
+  color: #048741;
+  transition: all 0.3s;
+  height: 42px;
+  font-family: 'Manrope', sans-serif;
+  font-size: 16px;
 `;
 
 const StyledP = styled('p')`
@@ -221,11 +257,36 @@ const StyledSecondModal = styled(Box)`
   transform: translate(-50%, -50%);
   width: 659px;
   height: 468px;
-  background-color: white;
+  background-color: #ebf2fc;
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
   border-radius: 20px;
   padding: 16px;
+
+  h2 {
+    font-family: 'Manrope', sans-serif;
+    font-weight: 500;
+    width: 380px;
+    height: 50px;
+    font-size: 27px;
+    margin-bottom: 50px;
+  }
+
+  p {
+    font-family: 'Manrope', sans-serif;
+    font-weight: 400;
+    font-size: 18px;
+    margin-bottom: 10px;
+  }
+`;
+
+const StyledParagraf = styled('p')`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: -30px;
+  margin-bottom: 2rem;
 `;
