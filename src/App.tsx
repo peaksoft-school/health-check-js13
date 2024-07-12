4b829562d01de829eb54c4e0e73f8ec48af0e98e
Syimyk <syjmyk907@gmail.com>
2024-07-12 21:09:58 +0600

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { FC } from 'react';
-import OurService from './components/landingPage/OurService';
 // import Switcher from "./components/UI/CustomUI/Switcher";
 
 const App: FC = () => {
   return (
-    <div>
-      {/* <Switcher label="Create"  labelPlacement="bottom" /> */}
-      <OurService />
-    </div>
+    <div>{/* <Switcher label="Create"  labelPlacement="bottom" /> */}</div>
   );
 };
 
