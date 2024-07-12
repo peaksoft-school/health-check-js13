b9c0a3c59658154f65c976f188eb49ae2346c46a
AmanturDev11 <ismailovamantur22@gmail.com>
2024-07-12 17:10:49 +0600

    App.tsx Health check

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
-// import { FC } from 'react';
+import { FC } from 'react';
 
-// const App: FC = () => <h1>Health check</h1>;
+const App: FC = () => <h1>Health check</h1>;
 
-// export default App;
-import React from 'react';
-import Header from './layout/user/Header';
-
-const App = () => {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
-};
 
 export default App;
