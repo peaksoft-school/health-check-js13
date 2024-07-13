92b7b7cbe5b05f5cce82704e3e613b17d020db2a
Syimyk <syjmyk907@gmail.com>
2024-07-13 19:51:38 +0600

    create aplication

--- src/components/landingPage/Aplication.tsx
+++ src/components/landingPage/Aplication.tsx
@@ -1,8 +1,16 @@
+import { Box, styled } from '@mui/material';
 
 const Aplication = () => {
-  return (
-    <div>Aplication</div>
-  )
-}
+  return <Container></Container>;
+};
 
-export default Aplication
+export default Aplication;
+
+const Container = styled(Box)({
+  width: '100%',
+  minWidth: '75rem',
+  maxWidth: '1440px',
+  border: '1px solid black',
+  minHeight: '500px',
+  margin: '20px auto',
+});
