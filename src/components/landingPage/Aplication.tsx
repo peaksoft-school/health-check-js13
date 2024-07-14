69f585ced22e59cf196a2366320d766fb7e1374b
Syimyk <syjmyk907@gmail.com>
2024-07-14 20:46:15 +0600

    create application component

--- src/components/landingPage/Aplication.tsx
+++ src/components/landingPage/Aplication.tsx
@@ -3,6 +3,7 @@ import Input from '../UI/Input';
 import MaleFemaleIcon from '../../assets/icons/MaleFemaleIcon.svg';
 import CallProgressIcon from '../../assets/icons/CallProgressIcon.svg';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import GirlDoctor from '../../assets/images/GirlDoctor.png';
 interface IAplicationProps {
   name?: string;
   number?: number;
@@ -27,34 +28,57 @@ const Aplication = ({ updateFunc }: AplicationProps) => {
       <AplicationBox>
         <p className="h1text">Оставьте заявку</p>
         <p className="h3text">
-          Оставьте свой номер и наши специалисты свяжутся с Вами в ближайшее
-          время
+          Оставьте свой номер и наши специалисты свяжутся с Вами
+          <br />
+          в ближайшее время
         </p>
-        <BoxForm component={'form'}>
+        <BoxForm component={'form'} onSubmit={handleSubmit(onSubmit)}>
           <div className="inputBox">
-            <Input
-              fullWidth
-              size="small"
-              icon={MaleFemaleIcon}
-              label="Как к Вам обратиться?"
-              placeholder="Введите имя"
-            />
-            <Input
-              fullWidth
-              size="small"
-              icon={CallProgressIcon}
-              label="Номер мобильного телефона"
-              placeholder="+996 (___) __-__-__"
-            />
+            <div className="inBox">
+              <Input
+                {...register('name', {
+                  required: 'Обязательное поле',
+                })}
+                fullWidth
+                size="small"
+                icon={MaleFemaleIcon}
+                label="Как к Вам обратиться?"
+                placeholder="Введите имя"
+                className="inputsCor"
+                error={!!errors.name}
+                helperText={errors.name?.message ? errors.name.message : ''}
+              />
+            </div>
+            <div className="inBox">
+              <Input
+                {...register('number', {
+                  required: 'Ведите ваш действительный номер',
+                  pattern: {
+                    value: /^\+996\d{9}$/,
+                    message: 'Введите корректный номер телефона',
+                  },
+                })}
+                fullWidth
+                size="small"
+                icon={CallProgressIcon}
+                label="Номер мобильного телефона"
+                placeholder="+996 (___) __-__-__"
+                className="inputsCor"
+                error={!!errors.number}
+                helperText={errors.number?.message ? errors.number.message : ''}
+              />
+            </div>
           </div>
           <div className="buttonBox">
-            <MuiButton size="large" variant="contained">
+            <MuiButton size="large" type="submit" variant="contained">
               ОТПРАВИТЬ ЗАЯВКУ
             </MuiButton>
           </div>
         </BoxForm>
       </AplicationBox>
-      <BoxMui></BoxMui>
+      <BoxMui>
+        <img className="imgGirl" src={GirlDoctor} alt="GirlDoctor" />
+      </BoxMui>
     </Container>
   );
 };
@@ -63,18 +87,18 @@ export default Aplication;
 
 const Container = styled(Box)(() => ({
   width: '100%',
-  minWidth: '75rem',
   maxWidth: '1440px',
-  border: '1px solid black',
+  minWidth: '1200px',
   minHeight: '500px',
   margin: '20px auto',
   display: 'flex',
-  alignItems: 'center',
+  alignItems: 'end',
+  justifyContent: 'center',
+  fontFamily: 'Manrope, sans-serif',
 }));
 const AplicationBox = styled(Box)(() => ({
   width: '42em',
   height: '28.75em',
-  border: '1px solid black',
   backgroundColor: '#DBEBFF',
   borderRadius: '20px',
   display: 'flex',
@@ -89,29 +113,40 @@ const AplicationBox = styled(Box)(() => ({
   },
   '.h3text': {
     textAlign: 'center',
-    fontSize: '20px',
+    fontSize: '17px',
     fontWeight: '200',
   },
 }));
 const BoxMui = styled(Box)(() => ({
   width: '36.5rem',
   height: '33.1rem',
-  border: '1px solid black',
+  position: 'relative',
+  '.imgGirl': {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    left: '-80px',
+  },
 }));
 
 const BoxForm = styled(Box)(() => ({
-  border: '1px solid black',
   display: 'flex',
   flexDirection: 'column',
-  padding: '5px',
+  alignItems: 'center',
+  padding: '10px',
+
   '.inputBox': {
     margin: '10px',
     display: 'flex',
-    alignItems: 'stretch',
-    gap: '20px',
-    width: '100%',
-    border: '1px solid black',
-
+    justifyContent: 'center',
+    gap: '10px',
+    width: '90%',
+    '.inBox': {
+      width: '70%',
+    },
+    '.inputsCor': {
+      backgroundColor: '#fff',
+    },
   },
   '.buttonBox': {
     width: '100%',
@@ -127,13 +162,13 @@ const MuiButton = styled(Button)(() => ({
     color: 'white',
     position: 'relative',
     height: '50px',
-    fontSize: '12px',
-    width:"250px",
+    fontSize: '14px',
+    width: '250px',
     '&:after': {
       content: 'url(src/assets/icons/arrow.svg)',
       marginTop: '8px',
-      marginLeft: '10px',
-      marginRight: '-13px',
+      marginLeft: '20px',
+      marginRight: '-33px',
     },
   },
 }));
