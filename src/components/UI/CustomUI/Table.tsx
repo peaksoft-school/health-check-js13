a28b0fac61b78900d3cd4d34965f4581e99ce892
Syimyk <syjmyk907@gmail.com>
2024-07-15 16:30:09 +0600

    fixed bug

--- src/components/UI/CustomUI/Table.tsx
+++ src/components/UI/CustomUI/Table.tsx
@@ -1,7 +1,76 @@
-import React from 'react';
+import { useTable } from 'react-table';
+import { FC, useMemo } from 'react';
+import { Paper, Box, styled } from '@mui/material';
 
-const Table = () => {
-  return <div>Table</div>;
+type TypesPropsTable = {
+  column: any[];
+  data: any[];
+};
+
+const Table: FC<TypesPropsTable> = ({ column, data }) => {
+  const tableData = useMemo(() => data, [data]);
+  const columns = useMemo(() => column, [column]);
+
+  const tableInstance = useTable({
+    columns,
+    data: tableData,
+  });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
+  return (
+    <TableContainerStyled component={Paper}>
+      <TableStyled {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => (
+                  <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </TableStyled>
+    </TableContainerStyled>
+  );
 };
 
 export default Table;
+
+const TableContainerStyled = styled(Box)({
+  width: '1200px',
+  margin: '0 auto',
+  minHeight: '746px',
+});
+
+const TableStyled = styled('table')({
+  width: '100%',
+  borderCollapse: 'collapse',
+  '& thead th': {
+    borderBottom: '1px solid #ddd',
+    padding: '1rem',
+    textAlign: 'left',
+    fontSize: '13px',
+  },
+  '& tbody td': {
+    borderBottom: '1px solid #ddd',
+    padding: '10px 10px 10px 15px',
+    fontSize: '12px',
+  },
+  '& tbody tr:hover': {
+    backgroundColor: '#f1f1f1',
+  },
+});
---
+++ src/utils/constants/Column.tsx
@@ -0,0 +1,239 @@
+import { Box } from '@mui/material';
+import Korzina from '../../assets/icons/Korzina.svg';
+import Switcher from '../../components/UI/CustomUI/Switcher';
+import UpdateIcon from '../../assets/icons/UpdateIcon.svg';
+import Table from '../../components/UI/CustomUI/Table';
+import status from '../constants/status.json';
+export const TableOne = [
+  {
+    Header: (
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '5px',
+          cursor: 'pointer',
+        }}>
+        <Switcher />
+        <img src={Korzina} alt="delete" />
+      </Box>
+    ),
+    accessor: 'hello',
+    Cell: () => <Switcher />,
+  },
+  {
+    Header: '№',
+    accessor: 'id',
+  },
+  {
+    Header: 'Имя и фамилия',
+    accessor: 'first_name',
+  },
+  {
+    Header: 'Номер телефона',
+    accessor: 'phone',
+  },
+  {
+    Header: 'Почта',
+    accessor: 'email',
+  },
+  {
+    Header: 'Выбор услуги',
+    accessor: 'service',
+  },
+  {
+    Header: 'Выбор специалиста',
+    accessor: 'addService',
+  },
+  {
+    Header: 'Дата и время',
+    accessor: 'date',
+  },
+  {
+    Header: 'Обработан',
+    accessor: 'progress',
+    Cell: () => (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'end',
+          cursor: 'pointer',
+        }}>
+        <Switcher />
+      </div>
+    ),
+  },
+  {
+    Header: 'Action',
+    accessor: 'and',
+    Cell: () => (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'end',
+          cursor: 'pointer',
+        }}>
+        <img src={Korzina} alt="delete" />
+      </div>
+    ),
+  },
+];
+export const StatusHeader = [
+  {
+    Header: '№',
+    accessor: 'id',
+  },
+  {
+    Header: 'Статус',
+    accessor: 'status',
+    Cell: () => <Switcher />,
+  },
+  {
+    Header: 'Специалист',
+    accessor: 'name',
+    Cell: ({ cell: { value } }: any) => (
+      <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+        {typeof value === 'object' ? (
+          <>
+            <img
+              src={value.img}
+              alt="specialist"
+              style={{ width: '40px', height: '40px', cursor: 'pointer' }}
+            />
+            <div>
+              <div>{value.name}</div>
+              <div style={{ fontSize: '13px', color: 'gray' }}>
+                {value.professi}
+              </div>
+            </div>
+          </>
+        ) : (
+          <div>{value}</div>
+        )}
+      </div>
+    ),
+  },
+  {
+    Header: 'Отделение',
+    accessor: 'deport',
+  },
+  {
+    Header: 'Расписание',
+    accessor: 'date',
+  },
+  {
+    Header: 'Действие',
+    accessor: 'actions',
+    Cell: () => (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: 20,
+        }}>
+        <img
+          style={{ display: 'block', cursor: 'pointer' }}
+          src={UpdateIcon}
+          alt="edit"
+        />
+        <img
+          style={{ display: 'block', cursor: 'pointer' }}
+          src={Korzina}
+          alt="delete"
+        />
+      </div>
+    ),
+  },
+];
+export const TableTwo = [
+  {
+    Header: <Switcher />,
+    accessor: 'nello',
+    Cell: () => <Switcher />,
+  },
+  {
+    Header: '№',
+    accessor: 'id',
+  },
+  {
+    Header: 'Имя',
+    accessor: 'first_name',
+  },
+  {
+    Header: 'Дата',
+    accessor: 'date',
+  },
+  {
+    Header: 'Номер Телефона',
+    accessor: 'phone',
+  },
+  {
+    Header: 'Обработан',
+    accessor: 'processing',
+    Cell: () => (
+      <div
+        style={{
+          marginLeft: '26px',
+          cursor: 'pointer',
+        }}>
+        <Switcher />
+      </div>
+    ),
+  },
+  {
+    Header: 'Действия',
+    accessor: '',
+    Cell: () => (
+      <div
+        style={{
+          marginLeft: '26px',
+          cursor: 'pointer',
+        }}>
+        <img src={Korzina} alt="delete" />
+      </div>
+    ),
+  },
+];
+export const TableThree = [
+  {
+    Header: '№',
+    accessor: 'id',
+  },
+  {
+    Header: 'Имя Фамилия',
+    accessor: 'first_name',
+  },
+  {
+    Header: 'Номер телефона',
+    accessor: 'phone',
+  },
+  {
+    Header: 'Почта',
+    accessor: 'email',
+  },
+  {
+    Header: 'Дата сдачи',
+    accessor: 'date',
+  },
+  {
+    Header: 'Действия',
+    accessor: 'srfds',
+    Cell: (
+      <div
+        style={{
+          marginLeft: '26px',
+          cursor: 'pointer',
+        }}>
+        <img src={Korzina} alt="delete" />
+      </div>
+    ),
+  },
+];
+const Column = () => {
+  return <Table column={StatusHeader} data={status} />;
+};
+
+export default Column;
