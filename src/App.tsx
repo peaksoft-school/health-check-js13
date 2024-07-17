77fb2be63ee9e79e0c1a6b4ee33c3ab8ba688d10
Syimyk <syjmyk907@gmail.com>
2024-07-17 18:16:53 +0600

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import AboutClinic from './components/landingPage/AboutClinic';
 
-const App: FC = () => <AboutClinic/>;
+const App: FC = () => <AboutClinic />;
 
 export default App;
