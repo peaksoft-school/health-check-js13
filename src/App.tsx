53820c49eef24c09eb246836f89792e316b348a6
Syimyk <syjmyk907@gmail.com>
2024-07-26 21:21:08 +0600

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,3 @@
-import { FC } from 'react';
-import Header from './layout/user/Header';
-import AboutClinic from './components/landingPage/AboutClinic';
-import AboutPart from './components/landingPage/AboutPart';
-import Aplication from './components/landingPage/Aplication';
-import Doctors from './components/landingPage/Doctors';
-import OurService from './components/landingPage/OurService';
-
-const App: FC = () => (
-  <div>
-    <Header />
-    <AboutClinic />
-    <AboutPart />
-    <Aplication />
-    <Doctors />
-    <OurService />
-  </div>
-);
+const App = () => <h1>Health Check</h1>;
 
 export default App;
