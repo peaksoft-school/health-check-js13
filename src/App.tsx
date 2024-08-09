0b1ee2e58a56795176a27ed972854aa20baaeb38
koichumanovernest <koichumanovernest@gmail.com>
2024-08-09 11:19:41 +0600

    create adminHeader

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,4 @@
 
-import { Welcome } from './components/landingPage/Welcome';
-import Footer from './layout/user/Footer';
-import Header from './layout/user/Header';
-
-const App = () => (
-  <div>
-    <Header />
-    <Welcome />
-    <Footer />
-  </div>
-);
+const App = () => <h1>Healts-Chek</h1>
 
 export default App;
