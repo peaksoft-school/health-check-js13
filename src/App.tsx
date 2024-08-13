15721e27ac19fd8e79297cc1d1f2c441c6caed35
Syimyk <syjmyk907@gmail.com>
2024-08-13 16:59:25 +0600

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { FC } from 'react';
-import AboutClinic from './components/landingPage/AboutClinic';
-
-const App: FC = () => <AboutClinic/>;
+import Table from './components/UI/Table';
+import { applicationHeader } from './utils/constants/Column';
+import applicationBody from './utils/constants/applicationBody.json';
+const App: FC = () => (
+  <Table data={applicationBody} columns={applicationHeader} />
+);
 
 export default App;
