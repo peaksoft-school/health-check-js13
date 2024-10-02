fa0b137f4b75a0c8dfe30f7fd923254a69f1abe2
Syimyk <syjmyk907@gmail.com>
2024-10-02 16:19:40 +0600

    fixing admin header

--- src/layout/admin/AdminHeader.tsx
+++ src/layout/admin/AdminHeader.tsx
@@ -15,9 +15,7 @@ import { useAppDispatch } from '../../hooks/customHooks';
 import { logout } from '../../store/slices/auth/authSlice';
 
 const AdminHeader: FC = () => {
-  const [activeButton, setActiveButton] = useState<string>('Заявки');
   const [adminOption, setAdminOption] = useState<string>('Администратор'); // состояние для Select
-  const handleButtonClick = (text: string) => () => setActiveButton(text);
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
@@ -53,10 +51,8 @@ const AdminHeader: FC = () => {
       <StyledMapContainer>
         {links.map(link => (
           <Box key={link.value}>
-            <HeaderButton
-              active={activeButton === link.value}
-              onClick={handleButtonClick(link.value)}>
-              <NavLinkStyled to={link.to} active={activeButton === link.value}>
+            <HeaderButton>
+              <NavLinkStyled to={link.to} end>
                 {link.value}
               </NavLinkStyled>
             </HeaderButton>
@@ -68,7 +64,7 @@ const AdminHeader: FC = () => {
           style={{ width: '180px', border: 'none' }}
           options={options}
           onChange={handlerChangeHome}
-          value={adminOption} // Значение, которое будет отображаться
+          value={adminOption}
         />
       </Box>
     </StyledContainer>
@@ -77,28 +73,32 @@ const AdminHeader: FC = () => {
 
 export default AdminHeader;
 
-const NavLinkStyled = styled(NavLink)<{ active?: boolean }>(({ active }) => ({
+const NavLinkStyled = styled(NavLink)(() => ({
   textDecoration: 'none',
-  color: active ? '#252525' : '#808080',
+  color: '#808080',
   transition: 'color 0.3s ease, font-weight 0.3s ease',
   position: 'relative',
-  '&:after': active
-    ? {
-        content: '""',
-        position: 'absolute',
-        left: '0',
-        bottom: '-2px',
-        width: '100%',
-        height: '2px',
-        transition: 'width 0.3s ease, left 0.3s ease',
-      }
-    : {
-        content: '""',
-        position: 'absolute',
-        left: '50%',
-        bottom: '-2px',
-        width: '0',
-        height: '2px',
-        transition: 'width 0.3s ease, left 0.3s ease',
-      },
+  '&.active': {
+    color: '#252525',
+    '&:after': {
+      content: '""',
+      position: 'absolute',
+      left: '0',
+      bottom: '-33px',
+      width: '100%',
+      height: '2px',
+      backgroundColor: '#048741',
+      transition: 'width 0.3s ease, left 0.3s ease',
+    },
+  },
+  '&:after': {
+    content: '""',
+    position: 'absolute',
+    left: '50%',
+    bottom: '-33px',
+    width: '0',
+    height: '2px',
+    backgroundColor: '#252525',
+    transition: 'width 0.3s ease, left 0.3s ease',
+  },
 }));
